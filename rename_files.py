c793fd2c7af3166aecf6db43f3331666f8e188c2
Liam Millar <me@liammillar.ie>
2018-01-25 21:42:17 +0000

    Add print filelist

--- rename_files.py
+++ rename_files.py
@@ -2,8 +2,8 @@ import os
 def rename_files():
 
     # (1) Get file names from a folder
-    os.listdir("C:\Python27\apps\test")
-
+    file_list = os.listdir(r"C:\Python27\apps\test")
+    print (file_list)
     # (2) for each file, rename filename
 
 rename_files()
