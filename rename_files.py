1c2f52358533d341a2cc4b8e9674d8dd9d5b34f6
Liam Millar <me@liammillar.ie>
2018-01-25 22:27:35 +0000

    Add for each file rename_files

--- rename_files.py
+++ rename_files.py
@@ -2,8 +2,16 @@ import os
 def rename_files():
 
     # (1) Get file names from a folder
-    file_list = os.listdir(r"C:\Python27\apps\test")
+    test_path = r"C:\Python27\apps\test"
+    file_list = os.listdir(test_path)
     print (file_list)
+    saved_path = os.getcwd()
+    os.chdir(test_path)
+    
     # (2) for each file, rename filename
+    
+    for file_name in file_list:
+       os.rename(file_name,file_name.translate(None, "0123456789"))
 
+    os.chdir(saved_path)
 rename_files()
